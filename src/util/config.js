5802802f30fb9f939e20884fee94176ee2a12e6b
cai-peiyuan <262114450@qq.com>
2018-07-17 20:15:36 +0800

    添加api_config配置。前端使用api需添加api_config配置项

--- src/util/config.js
+++ src/util/config.js
@@ -7,32 +7,12 @@ type Config = {|
 |};
 
 const config: Config = {
-    API_URL: 'http://mapabc0.pinganzhongsheng.com:6060/mapserver',
+   // API_URL: 'http://mapabc0.pinganzhongsheng.com:6060/mapserver',
+    API_URL: api_config.url,
     REQUIRE_ACCESS_TOKEN: true,
     ACCESS_TOKEN: null,
-    FONT_DOMAINS : [
-    	'http://mapabc0.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc1.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc2.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc3.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc4.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc5.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc6.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc7.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc8.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc9.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc19.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc10.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc11.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc12.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc13.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc14.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc15.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc16.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc17.pinganzhongsheng.com:6060/mapserver',
-    	'http://mapabc18.pinganzhongsheng.com:6060/mapserver'
-    ]
-
+   // FONT_URL : [ 'http://mapabc0.pinganzhongsheng.com:6060/mapserver' ]
+    FONT_URL : api_config.font_url
 };
 
 export default config;
